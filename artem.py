555c0a7a82594783c8f494c8109aca5feb411813
artik2287 <153529076+artik2287@users.noreply.github.com>
2023-12-11 17:01:55 +0200

    add file with program

---
+++ artem.py
@@ -0,0 +1,18 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import*
+
+app=QApplication([])
+win=QWidget()
+
+win.resize(500,500)
+win.setWindowTitle('Тест ГітХаб')
+
+title=QLabel('Тестовий додаток для гібХаб')
+
+line_h=QHBoxLayout()
+line_h.addWidget(title)
+
+win.setLayout(line_h)
+
+win.show()
+app.exec_()
